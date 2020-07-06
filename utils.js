c737611e3922dca7a0fa3a8106afc6653a30f22a
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-07-06 10:06:18 +0200

    Add mapping from extend name to import name

--- utils.js
+++ utils.js
@@ -1,3 +1,43 @@
+const NAME_MAP = {
+  'airbnb-base': 'eslint-config-airbnb-base',
+  airbnb: 'eslint-config-airbnb',
+  'plugin:import/errors': ['eslint-plugin-import', 'configs', 'errors'],
+  'plugin:import/warnings': ['eslint-plugin-import', 'configs', 'warnings'],
+  'plugin:react/recommended': ['eslint-plugin-react', 'configs', 'recommended'],
+  'plugin:jsx-a11y/recommended': [
+    'eslint-plugin-jsx-a11y',
+    'configs',
+    'recommended',
+  ],
+  'plugin:@typescript-eslint/recommended': [
+    '@typescript-eslint/eslint-plugin',
+    'configs',
+    'recommended',
+  ],
+};
+
+const recursiveResolve = (obj, keyArray) => {
+  if (!obj) {
+    return obj;
+  }
+  if (!keyArray || keyArray.length === 0) {
+    return obj;
+  }
+  const nextKey = keyArray.shift();
+  return recursiveResolve(obj[nextKey], keyArray);
+};
+
+const doImport = (name) => {
+  const packageName = NAME_MAP[name] || name;
+  if (typeof packageName === 'string') {
+    return require(packageName);
+  } else {
+    const moduleName = packageName.shift();
+    const module = require(moduleName);
+    return recursiveResolve(module, packageName);
+  }
+};
+
 const flattenEslintConfig = (config) => {
   // Accepts an eslint config. Loops through its extends, and returns a flattened version
   if (!config.extends || config.extends.length === 0) {
@@ -6,9 +46,8 @@ const flattenEslintConfig = (config) => {
   let rules = config.rules || {};
   const extend = config.extends || [];
   for (const confPath of extend) {
-    console.log(`Flattening ${confPath}`);
-    const nextConfig = require(confPath);
-    rules = {...flattenEslintConfig(nextConfig), ...rules};
+    const nextConfig = flattenEslintConfig(doImport(confPath));
+    rules = { ...rules, ...nextConfig.rules };
   }
   return {
     ...config,
