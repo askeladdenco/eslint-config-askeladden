448d44743a24c56fe5e00c4de5a15f6f10ce7e3d
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-08-20 00:06:33 +0200

    javascript: add jest plugin

--- javascript/index.js
+++ javascript/index.js
@@ -3,5 +3,8 @@ const jsRules = require('./rules');
 module.exports = {
   extends: ['airbnb-base', 'plugin:import/errors', 'plugin:import/warnings'],
   parser: 'babel-eslint',
+  plugins: [
+    "jest"
+  ],
   rules: jsRules,
 };
