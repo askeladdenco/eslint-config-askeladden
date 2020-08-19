581fdb48ce86a6ec708080b4f90c294d4dcb8043
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-08-19 23:46:54 +0200

    prettier: turn off linting for prettier-fixable rules
    There is no need for the IDE to hint about linting to the programmer.
    When using prettier, this is not relevant for them as it is programatically fixed.

--- prettier/index.js
+++ prettier/index.js
@@ -2,6 +2,10 @@ module.exports = {
   extends: [
     'plugin:prettier/recommended', // enables-eslint-plugin-prettier, sets the prettier/prettier rule to "error", extends eslint-config-prettier so that we can set prettier options in .prettierrc
   ],
+  rules: {
+    // Prettier errors are expected to be fixed automatically. No need to also hint in IDE.
+    "prettier/prettier": 0
+  },
   // "plugins": [
   //   "prettier", enabled by the plugin;prettier/recommneded extension
   // ],
