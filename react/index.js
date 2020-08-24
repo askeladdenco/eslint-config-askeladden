24056f6b89ea79c86839500da71b3008568cc6e9
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-08-24 23:14:45 +0200

    react: include airbnb/hooks

--- react/index.js
+++ react/index.js
@@ -3,6 +3,7 @@ const jsRules = require('../javascript/rules');
 module.exports = {
   extends: [
     'airbnb',
+    'airbnb/hooks',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
   ],
